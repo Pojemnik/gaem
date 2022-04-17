f7c3cadd4295b96445112a79728b26a2675c0879
Pojemnik <andriej194@gmail.com>
2022-04-17 21:52:04 +0200

    Add libraries initalization code

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,69 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm.hpp>
+
+#include <iostream>
+#include "main.h"
+
+void freeResources()
+{
+
+}
+
+void initResources()
+{
+
+}
+
+void errorCallback(int error, const char* description)
+{
+    std::cerr << description << std::endl;
+}
+
+void draw(GLFWwindow* window)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+}
 
 int main(void)
 {
     GLFWwindow* window;
-
-    /* Initialize the library */
+    glfwSetErrorCallback(errorCallback);
     if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
     {
-        glfwTerminate();
-        return -1;
+        std::cerr << "GLFW initialization error" << std::endl;
+        return 1;
     }
 
-    /* Make the window's context current */
+    window = glfwCreateWindow(640, 480, "Gaem", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Window creation error" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    GLenum glewInitResult = glewInit();
+    if(glewInitResult != GLEW_OK)
+    {
+        std::cerr << "GLEW initialization error: "  << glewInitResult << std::endl;
+        return 1;
+    }
+    initResources();
+
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
+        float time = glfwGetTime();
+        glfwSetTime(0);
+        draw(window);
         glfwPollEvents();
     }
 
+    freeResources();
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
